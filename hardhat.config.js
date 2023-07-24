8e927f4f8cca49f018db8f4a84a6457f0a2fcdbb
mjlover05 <93177731+mjlover05@users.noreply.github.com>
2023-07-25 00:16:23 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,51 @@
 require("@nomicfoundation/hardhat-toolbox");
+//require('@openzeppelin/hardhat-upgrades');
+
+const FORK_FUJI = true;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+        // YOUR PRIVATE KEY HERE
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        // YOUR PRIVATE KEY HERE
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: "ABCDE12345ABCDE12345ABCDE123456789",
+  },
+
 };
